26e04a0cdd63e31ac901ce9693efb3fb6e394481
Jeremy Soller <jackpot51@gmail.com>
2017-08-30 10:11:19 -0600

    Fix EFI not having correct fstab

--- bash/install_bios.sh
+++ bash/install_bios.sh
@@ -7,15 +7,17 @@ then
 fi
 SQUASHFS="$(realpath "$1")"
 
+IMAGE=bios.img
+
 set -ex
 
-dd if=/dev/zero of=bios.img bs=1G count=8
-parted -s bios.img mklabel msdos
-parted -s bios.img print
-parted -s bios.img mkpart primary ext4 0% 100%
-parted -s bios.img print
+dd if=/dev/zero of="${IMAGE}" bs=1G count=8
+parted -s "${IMAGE}" mklabel msdos
+parted -s "${IMAGE}" print
+parted -s "${IMAGE}" mkpart primary ext4 0% 100%
+parted -s "${IMAGE}" print
 
-LO="$(sudo losetup --find --partscan --show bios.img)"
+LO="$(sudo losetup --find --partscan --show "${IMAGE}")"
 
 sudo mkfs.ext4 "${LO}p1"
 
@@ -34,7 +36,8 @@ sudo chroot "${DIR}/" apt autoremove -y --purge
 
 ROOTDEV="$(sudo chroot "${DIR}/" df --output=source / | sed 1d)"
 ROOTUUID="$(sudo chroot "${DIR}/" blkid -o value -s UUID "${ROOTDEV}")"
-echo "UUID=${ROOTUUID} / ext4 errors=remount-ro 0 1" | sudo chroot "${DIR}/" tee /etc/fstab
+echo "# / was on ${ROOTDEV} during installation" | sudo chroot "${DIR}/" tee /etc/fstab
+echo "UUID=${ROOTUUID} / ext4 errors=remount-ro 0 1" | sudo chroot "${DIR}/" tee -a /etc/fstab
 
 sudo chroot "${DIR}/" grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -44,7 +47,7 @@ sudo umount "${DIR}/dev"
 sudo umount "${DIR}/proc"
 sudo umount "${DIR}/sys"
 
-sudo umount "${DIR}"
+sudo umount "${DIR}" || sudo umount -lf "${DIR}"
 
 rmdir "${DIR}"
 
--- bash/install_efi.sh
+++ bash/install_efi.sh
@@ -7,16 +7,18 @@ then
 fi
 SQUASHFS="$(realpath "$1")"
 
+IMAGE=efi.img
+
 set -ex
 
-dd if=/dev/zero of=efi.img bs=1G count=8
-parted -s efi.img mklabel gpt
-parted -s efi.img print
-parted -s efi.img mkpart primary fat32 0% 256M
-parted -s efi.img mkpart primary ext4 256M 100%
-parted -s efi.img print
+dd if=/dev/zero of="${IMAGE}" bs=1G count=8
+parted -s "${IMAGE}" mklabel gpt
+parted -s "${IMAGE}" print
+parted -s "${IMAGE}" mkpart primary fat32 0% 256M
+parted -s "${IMAGE}" mkpart primary ext4 256M 100%
+parted -s "${IMAGE}" print
 
-LO="$(sudo losetup --find --partscan --show efi.img)"
+LO="$(sudo losetup --find --partscan --show "${IMAGE}")"
 
 sudo mkfs.fat -F 32 "${LO}p1"
 sudo mkfs.ext4 "${LO}p2"
@@ -40,12 +42,12 @@ sudo chroot "${DIR}/" apt autoremove -y --purge
 ROOTDEV="$(sudo chroot "${DIR}/" df --output=source / | sed 1d)"
 ROOTUUID="$(sudo chroot "${DIR}/" blkid -o value -s UUID "${ROOTDEV}")"
 echo "# / was on ${ROOTDEV} during installation" | sudo chroot "${DIR}/" tee /etc/fstab
-echo "UUID=${ROOTUUID} / ext4 errors=remount-ro 0 1" | sudo chroot "${DIR}/" tee /etc/fstab
+echo "UUID=${ROOTUUID} / ext4 errors=remount-ro 0 1" | sudo chroot "${DIR}/" tee -a /etc/fstab
 
 EFIDEV="$(sudo chroot "${DIR}/" df --output=source /boot/efi | sed 1d)"
 EFIUUID="$(sudo chroot "${DIR}/" blkid -o value -s UUID "${EFIDEV}")"
-echo "# /boot/efi was on ${EFIDEV} during installation" | sudo chroot "${DIR}/" tee /etc/fstab
-echo "UUID=${ROOTUUID} /boot/efi vfat umask=0077 0 1" | sudo chroot "${DIR}/" tee /etc/fstab
+echo "# /boot/efi was on ${EFIDEV} during installation" | sudo chroot "${DIR}/" tee -a /etc/fstab
+echo "UUID=${ROOTUUID} /boot/efi vfat umask=0077 0 1" | sudo chroot "${DIR}/" tee -a /etc/fstab
 
 sudo chroot "${DIR}/" grub-mkconfig -o /boot/grub/grub.cfg
 
