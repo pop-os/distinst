4484a2748b615a369dd4d094074b63f2c18fc95c
Michael Aaron Murphy <mmstickman@gmail.com>
2018-01-11 13:08:56 -0500

    Add Serial To Disk Information
    It's possible for users to unplug and swap hard drives after obtaining
    partition information. Therefore, to prevent issues from occuring as a
    result of that, disks will now contain serial number information which
    can be used as a unique identifier when commiting partition changes to
    disks.

--- src/disk/mod.rs
+++ src/disk/mod.rs
@@ -1,7 +1,9 @@
 mod mounts;
+mod serial;
 
 use libparted::{Device, Disk as PedDisk, Partition};
 use self::mounts::Mounts;
+use self::serial::get_serial_no;
 use std::io;
 use std::str;
 use std::path::{Path, PathBuf};
@@ -14,6 +16,7 @@ pub enum DiskError {
     DiskNew,
     DiskGet,
     MountsObtain { why: io::Error },
+    SerialGet { why: io::Error },
 }
 
 impl Display for DiskError {
@@ -24,7 +27,8 @@ impl Display for DiskError {
             DeviceProbe => writeln!(f, "unable to probe for devices"),
             DiskNew => writeln!(f, "unable to open disk"),
             DiskGet => writeln!(f, "unable to find disk"),
-            MountsObtain { ref why } => writeln!(f, "unable to get moutns: {}", why)
+            MountsObtain { ref why } => writeln!(f, "unable to get mounts: {}", why),
+            SerialGet { ref why } => writeln!(f, "unable to get serial number of device: {}", why),
         }
     }
 }
@@ -42,17 +46,16 @@ pub enum FileSystemType {
     Xfs,
 }
 
-
 #[derive(Debug, PartialEq, Clone, Copy, Hash)]
 pub enum PartitionType {
     Primary,
-    Logical
+    Logical,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy, Hash)]
 pub enum PartitionTable {
     Msdos,
-    Gpt
+    Gpt,
 }
 
 impl FileSystemType {
@@ -60,14 +63,14 @@ impl FileSystemType {
         let type_ = match string {
             "btrfs" => FileSystemType::Btrfs,
             "exfat" => FileSystemType::Exfat,
-            "ext2"  => FileSystemType::Ext2,
-            "ext3"  => FileSystemType::Ext3,
-            "ext4"  => FileSystemType::Ext4,
+            "ext2" => FileSystemType::Ext2,
+            "ext3" => FileSystemType::Ext3,
+            "ext4" => FileSystemType::Ext4,
             "fat16" => FileSystemType::Fat16,
             "fat32" => FileSystemType::Fat32,
             "linux-swap(v1)" => FileSystemType::Swap,
             "xfs" => FileSystemType::Xfs,
-            _ => return None
+            _ => return None,
         };
         Some(type_)
     }
@@ -81,6 +84,8 @@ impl FileSystemType {
 pub struct Disk {
     /// The model name of the device, assigned by the manufacturer.
     pub model_name: String,
+    /// A unique identifier to this disk.
+    pub serial: String,
     /// The location in the file system where the block device is located.
     pub device_path: PathBuf,
     /// The size of the disk in bytes.
@@ -98,9 +103,10 @@ pub struct Disk {
 }
 
 impl Disk {
-    pub fn new(device: &mut Device) -> Result<Disk, DiskError> {
+    fn new(device: &mut Device) -> Result<Disk, DiskError> {
         let model_name = String::from_utf8_lossy(device.model()).into();
         let device_path = device.path().to_owned();
+        let serial = get_serial_no(&device_path).map_err(|why| DiskError::SerialGet { why })?;
         let size = device.length();
         let sector_size = device.sector_size();
         let device_type = format!("{:?}", device.type_());
@@ -108,17 +114,15 @@ impl Disk {
 
         // Attempts to open the disk to obtain information regarding the partition table
         // and the partitions stored on the device.
-        let disk = PedDisk::new(device)
-            .map_err(|_| DiskError::DiskNew)?;
+        let disk = PedDisk::new(device).map_err(|_| DiskError::DiskNew)?;
 
         // Checks whether there is a partition table, and if so, which kind.
-        let table_type = disk.get_disk_type_name()
-            .and_then(|tn| match tn {
-                b"gpt" => Some(PartitionTable::Gpt),
-                b"msdos" => Some(PartitionTable::Msdos),
-                _ => None
-            });
-        
+        let table_type = disk.get_disk_type_name().and_then(|tn| match tn {
+            b"gpt" => Some(PartitionTable::Gpt),
+            b"msdos" => Some(PartitionTable::Msdos),
+            _ => None,
+        });
+
         // Determines if the table is of the msdos variable, as msdos partition
         // tables do not support partition names.
         let is_msdos = table_type.map_or(false, |tt| tt == PartitionTable::Msdos);
@@ -126,6 +130,7 @@ impl Disk {
         Ok(Disk {
             model_name,
             device_path,
+            serial,
             size,
             sector_size,
             device_type,
@@ -143,13 +148,40 @@ impl Disk {
                 partitions
             } else {
                 Vec::new()
-            }
+            },
         })
     }
 
-    pub fn from_name(name: &str) -> Result<Disk, DiskError> {
+    /// Obtains the disk that corresponds to a given device path.
+    ///
+    /// The `name` of the device should be a path, such as `/dev/sda`. If the device could
+    /// not be found, then `Err(DiskError::DeviceGet)` will be returned.
+    pub fn from_name<P: AsRef<Path>>(name: P) -> Result<Disk, DiskError> {
         let mut device = Device::get(name).map_err(|_| DiskError::DeviceGet)?;
         Disk::new(&mut device).map_err(|_| DiskError::DiskNew)
+    }
+
+    /// Obtains the disk that corresponds to a given serial number.
+    ///
+    /// First attempts to check if the supplied name has the valid serial number (highly likely),
+    /// then performs a full probe of all disks in the system to attempt to find the matching
+    /// serial number, in the event that the user swapped hard drive positions.
+    ///
+    /// If no match is found, then `Err(DiskError::DeviceGet)` is returned.
+    fn from_name_with_serial<P: AsRef<Path>>(name: P, serial: &str) -> Result<Disk, DiskError> {
+        Disk::from_name(name).and_then(|source| {
+            if &source.serial == serial {
+                Ok(source)
+            } else {
+                Disks::probe_devices().and_then(|disks| {
+                    disks
+                        .0
+                        .into_iter()
+                        .find(|disk| &disk.serial == serial)
+                        .ok_or(DiskError::DeviceGet)
+                })
+            }
+        })
     }
 
     pub fn path(&self) -> &Path {
@@ -179,23 +211,28 @@ impl PartitionInfo {
             part_type: match partition.type_get_name() {
                 "primary" => PartitionType::Primary,
                 "logical" => PartitionType::Logical,
-                _ => return Ok(None)
+                _ => return Ok(None),
             },
             mount_point: mounts.get_mount_point(&device_path),
             filesystem: partition.fs_type_name().and_then(FileSystemType::from),
             number: partition.num(),
-            name: if is_msdos { None } else { partition.name().map(String::from) },
+            name: if is_msdos {
+                None
+            } else {
+                partition.name().map(String::from)
+            },
             // Note that primary and logical partitions should always have a path.
             device_path,
             active: partition.is_active(),
             busy: partition.is_busy(),
             start_sector: partition.geom_start(),
-            end_sector: partition.geom_end()
+            end_sector: partition.geom_end(),
         }))
     }
 
     pub fn is_swap(&self) -> bool {
-        self.filesystem.map_or(false, |fs| fs == FileSystemType::Swap)
+        self.filesystem
+            .map_or(false, |fs| fs == FileSystemType::Swap)
     }
 
     pub fn path(&self) -> &Path {
--- src/disk/mounts.rs
+++ src/disk/mounts.rs
@@ -7,7 +7,8 @@ pub struct Mounts(Vec<MountInfo>);
 impl Mounts {
     pub fn new() -> io::Result<Mounts> {
         let mut mounts = File::open("/proc/mounts")?;
-        let mut buffer = String::with_capacity(mounts.metadata().ok().map_or(0, |m| m.len() as usize));
+        let mut buffer =
+            String::with_capacity(mounts.metadata().ok().map_or(0, |m| m.len() as usize));
         let _ = mounts.read_to_string(&mut buffer)?;
 
         let mut mounts = Vec::new();
@@ -16,25 +17,27 @@ impl Mounts {
             let device = fields.next().unwrap();
 
             // Skip devices which aren't tied to actual hardware.
-            if !device.contains("/") { continue }
+            if !device.contains("/") {
+                continue;
+            }
 
             mounts.push(MountInfo {
                 device: Path::new(&device).to_path_buf(),
-                mount_point: Path::new(&fields.next().unwrap()).to_path_buf()
+                mount_point: Path::new(&fields.next().unwrap()).to_path_buf(),
             })
         }
         Ok(Mounts(mounts))
     }
 
     pub fn get_mount_point(&self, path: &Path) -> Option<PathBuf> {
-        self.0.iter()
+        self.0
+            .iter()
             .find(|mount| &mount.device == path)
             .map(|mount| mount.mount_point.clone())
     }
 }
 
-
 pub struct MountInfo {
     device: PathBuf,
-    mount_point: PathBuf
+    mount_point: PathBuf,
 }
