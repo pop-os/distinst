0385707784e903996b5735c393f20e8c4bd5ed6c
Michael Aaron Murphy <mmstickman@gmail.com>
2018-01-18 23:38:30 -0500

    build.rs: only act if the crate type is 'dylib'
    If there's a compile error, you'll get an unhelpful error message about unparsed tokens.
    This will allow you to build the library and receive error messages if you build the bin or lib.
    This also will ensure that the dylib-related stuff doesn't execute when building just the bin/lib.

--- build.rs
+++ build.rs
@@ -6,21 +6,23 @@ use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    let target_path = PathBuf::from("target");
+	if cfg!(crate_type = "dylib") {
+		let target_path = PathBuf::from("target");
 
-    let pkg_config = format!(
-        include_str!("distinst.pc.in"),
-        name = env::var("CARGO_PKG_NAME").unwrap(),
-        description = env::var("CARGO_PKG_DESCRIPTION").unwrap(),
-        version = env::var("CARGO_PKG_VERSION").unwrap()
-    );
+		let pkg_config = format!(
+			include_str!("distinst.pc.in"),
+			name = env::var("CARGO_PKG_NAME").unwrap(),
+			description = env::var("CARGO_PKG_DESCRIPTION").unwrap(),
+			version = env::var("CARGO_PKG_VERSION").unwrap()
+		);
 
-    fs::create_dir_all(target_path.join("pkgconfig")).unwrap();
-    fs::File::create(target_path.join("pkgconfig").join("distinst.pc.stub")).unwrap()
-        .write_all(&pkg_config.as_bytes()).unwrap();
+		fs::create_dir_all(target_path.join("pkgconfig")).unwrap();
+		fs::File::create(target_path.join("pkgconfig").join("distinst.pc.stub")).unwrap()
+			.write_all(&pkg_config.as_bytes()).unwrap();
 
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+		let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(crate_dir).expect("unable to generate bindings")
-        .write_to_file(target_path.join("include").join("distinst.h"));
+		cbindgen::generate(crate_dir).expect("unable to generate bindings")
+			.write_to_file(target_path.join("include").join("distinst.h"));
+	}
 }
