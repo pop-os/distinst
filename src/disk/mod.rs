99be2caaed68c49fad2d47c142b6a0d89c1a43e2
Michael Aaron Murphy <mmstickman@gmail.com>
2018-01-25 16:00:12 -0700

    Add Sector::{Unit,Megabyte}FromEnd

--- src/disk/mod.rs
+++ src/disk/mod.rs
@@ -155,9 +155,13 @@ pub enum Sector {
     End,
     /// A raw value that directly corrects to the exact number of sectors that will be used.
     Unit(u64),
+    /// Similar to the above, but subtracting from the end.
+    UnitFromEnd(u64),
     /// Rather than specifying the sector count, the user can specify the actual size in megabytes.
     /// This value will later be used to get the exact sector count based on the sector size.
     Megabyte(u64),
+    /// Similar to the above, but subtracting from the end.
+    MegabyteFromEnd(u64),
     /// The percent can be represented by specifying a value between 0 and
     /// u16::MAX, where u16::MAX is 100%.
     Percent(u16),
@@ -315,11 +319,17 @@ impl Disk {
     /// Calculates the requested sector from a given `Sector` variant.
     pub fn get_sector(&self, sector: Sector) -> u64 {
         const MIB2: u64 = 2 * 1024 * 1024;
+
+        let end = || self.size - (MIB2 / self.sector_size);
+        let megabyte = |size| (size * 1_000_000) / self.sector_size;
+
         match sector {
             Sector::Start => MIB2 / self.sector_size,
-            Sector::End => self.size - (MIB2 / self.sector_size),
-            Sector::Megabyte(size) => (size * 1_000_000) / self.sector_size,
+            Sector::End => end(),
+            Sector::Megabyte(size) => megabyte(size),
+            Sector::MegabyteFromEnd(size) => end() - megabyte(size),
             Sector::Unit(size) => size,
+            Sector::UnitFromEnd(size) => end() - size,
             Sector::Percent(value) => {
                 ((self.size * self.sector_size) / ::std::u16::MAX as u64) * value as u64
                     / self.sector_size
