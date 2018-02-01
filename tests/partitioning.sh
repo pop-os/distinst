faeae33619135961d32f0b70563f5b4cb31bcb5a
Michael Aaron Murphy <mmstickman@gmail.com>
2018-02-01 15:41:40 -0500

    Improve partitioning integration test script

--- tests/partitioning.sh
+++ tests/partitioning.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-echo 'Running new partitioning tests'
+
+FS="tests/filesystem.squashfs"
+REMOVE="tests/filesystem.manifest-remove"
+
+if ! test -e "target/debug/distinst"; then
+    cargo build --bin distinst
+fi
+
+for file in $FS $REMOVE; do
+    if ! test -e "${file}"; then
+        echo "failed to find ${file}"
+        exit 1
+    fi
+done
+
 set -x
-index=0
-while test $index -ne 3; do
-    sudo target/debug/distinst -s test/filesystem.squashfs \
-        --test \
-        -r "test/filesystem.manifest-remove" \
+
+echo 'Running new partitioning tests'
+index=0; while test $index -ne 3; do
+    sudo target/debug/distinst --test \
+        -s "${FS}" \
+        -r "${REMOVE}" \
         -h "pop-testing" \
         -k "us" \
         -l "en_US.UTF-8" \
@@ -18,11 +33,10 @@ while test $index -ne 3; do
 done
 
 echo 'Running re-use partition tests'
-index=0
-while test $index -ne 3; do
-    sudo target/debug/distinst -s test/filesystem.squashfs \
-        --test \
-        -r "test/filesystem.manifest-remove" \
+index=0; while test $index -ne 3; do
+    sudo target/debug/distinst --test \
+        -s "${FS}" \
+        -r "${REMOVE}" \
         -h "pop-testing" \
         -k "us" \
         -l "en_US.UTF-8" \
