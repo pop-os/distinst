a22c00acbfeef3af33ff6bb1e380376ebd4361c4
Jeremy Soller <jackpot51@gmail.com>
2018-02-06 08:27:59 -0700

    Add loopback test

---
+++ tests/loopback.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DISK="tests/loopback.bin"
+
+if [ ! -e "$DISK" ]; then
+    dd if=/dev/zero of="$DISK" bs=1G count=8 status=progress
+fi
+
+set -x
+
+LO="$(sudo losetup --find "$DISK" --show --partscan)"
+
+./tests/install.sh "$LO"
+
+sudo losetup --detach "$LO"
