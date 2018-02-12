7f2d70b57c99aea05b3790e8125535cb2433cac8
Michael Aaron Murphy <mmstickman@gmail.com>
2018-02-12 11:03:02 -0500

    Simplify external command executions

--- src/disk/external.rs
+++ src/disk/external.rs
@@ -1,28 +1,34 @@
 //! A collection of external commands used throughout the program.
 
 use super::FileSystemType;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::io;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
-/// Checks & corrects errors with partitions that have been moved / resized.
-pub(crate) fn fsck<P: AsRef<Path>>(part: P, cmd: Option<(&str, &str)>) -> io::Result<()> {
-    let part = part.as_ref();
-    let status = Command::new(cmd.map_or("fsck", |(cmd, _)| cmd))
-        .arg(cmd.map_or("-fy", |(_, args)| args))
-        .arg(part)
+fn exec(cmd: &str, args: &[OsString]) -> io::Result<()> {
+    info!("libdistinst: executing {} with {:?}", cmd, args);
+
+    let status = Command::new(cmd)
+        .args(args)
         .stdout(Stdio::null())
+        .stderr(Stdio::null())
         .status()?;
+
     if status.success() {
-        info!("libdistinst: performed fsck on {}", part.display());
         Ok(())
     } else {
         Err(io::Error::new(
             io::ErrorKind::Other,
-            format!("fsck on {} failed with status: {}", part.display(), status),
+            format!("{} failed with status: {}", cmd, status),
         ))
     }
+}
+
+/// Checks & corrects errors with partitions that have been moved / resized.
+pub(crate) fn fsck<P: AsRef<Path>>(part: P, cmd: Option<(&str, &str)>) -> io::Result<()> {
+    let (cmd, arg) = cmd.unwrap_or(("fsck", "-fy"));
+    exec(cmd, &vec![arg.into(), part.as_ref().into()])
 }
 
 /// Utilized for ensuring that block & partition information has synced with the OS.
@@ -30,31 +36,17 @@ pub fn blockdev<P: AsRef<Path>, S: AsRef<OsStr>, I: IntoIterator<Item = S>>(
     disk: P,
     args: I,
 ) -> io::Result<()> {
-    let mut command = Command::new("blockdev");
-
-    command.args(args);
-    command.arg(disk.as_ref());
-
-    debug!("{:?}", command);
-
-    let status = command
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?;
-    if status.success() {
-        Ok(())
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            format!("blockdev failed with status: {}", status),
-        ))
-    }
+    exec("blockdev", &{
+        let mut args = args.into_iter()
+            .map(|x| x.as_ref().into())
+            .collect::<Vec<OsString>>();
+        args.push(disk.as_ref().into());
+        args
+    })
 }
 
 /// Formats the supplied `part` device with the file system specified.
 pub fn mkfs<P: AsRef<Path>>(part: P, kind: FileSystemType) -> io::Result<()> {
-    let part = part.as_ref();
-
     let (cmd, args): (&'static str, &'static [&'static str]) = match kind {
         FileSystemType::Btrfs => ("mkfs.btrfs", &["-f"]),
         FileSystemType::Exfat => ("mkfs.exfat", &[]),
@@ -70,25 +62,11 @@ pub fn mkfs<P: AsRef<Path>>(part: P, kind: FileSystemType) -> io::Result<()> {
         FileSystemType::Lvm(..) => unimplemented!(),
     };
 
-    let mut command = Command::new(cmd);
-    command.arg(part);
-    for arg in args {
-        command.arg(arg);
-    }
-
-    debug!("{:?}", command);
-
-    let status = command.stdout(Stdio::null()).status()?;
-
-    if status.success() {
-        info!("libdistinst: {} formatted with {:?}", part.display(), kind);
-        Ok(())
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            format!("mkfs for {:?} failed with status: {}", kind, status),
-        ))
-    }
+    exec(cmd, &{
+        let mut args = args.into_iter().map(Into::into).collect::<Vec<OsString>>();
+        args.push(part.as_ref().into());
+        args
+    })
 }
 
 pub fn vgcreate<I: Iterator<Item = S>, S: AsRef<OsStr>>(group: &str, devices: I) -> io::Result<()> {
