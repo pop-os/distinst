37a9abac81817b990f7dbbae7af9fa2d8a38b70f
Jeremy Soller <jeremy@system76.com>
2018-02-23 15:44:06 -0700

    Remove verbose disk_action functions

--- ffi/src/disk.rs
+++ ffi/src/disk.rs
@@ -44,26 +44,6 @@ pub unsafe extern "C" fn distinst_disk_destroy(disk: *mut DistinstDisk) {
     drop(Box::from_raw(disk as *mut Disk))
 }
 
-/// Converts a `DistinstDisk` into a `Disk`, executes a given action with that `Disk`,
-/// then converts it back into a `DistinstDisk`, returning the exit status of the function.
-unsafe fn disk_action<T, F: Fn(&Disk) -> T>(disk: *const DistinstDisk, action: F) -> T {
-    action(if disk.is_null() {
-        panic!("disk_action: disk is null")
-    } else {
-        &*(disk as *const Disk)
-    })
-}
-
-/// Converts a `DistinstDisk` into a `Disk`, executes a given action with that `Disk`,
-/// then converts it back into a `DistinstDisk`, returning the exit status of the function.
-unsafe fn disk_action_mut<T, F: Fn(&mut Disk) -> T>(disk: *mut DistinstDisk, action: F) -> T {
-    action(if disk.is_null() {
-        panic!("disk_action: disk is null")
-    } else {
-        &mut *(disk as *mut Disk)
-    })
-}
-
 #[no_mangle]
 pub unsafe extern "C" fn distinst_disk_get_device_path(
     disk: *const DistinstDisk,
@@ -113,14 +93,16 @@ pub unsafe extern "C" fn distinst_disk_list_partitions_destroy(
 pub unsafe extern "C" fn distinst_disk_get_sectors(
     disk: *const DistinstDisk
 ) -> libc::uint64_t {
-    disk_action(disk, |disk| disk.get_sectors())
+    let disk = &*(disk as *const Disk);
+    disk.get_sectors()
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn distinst_disk_get_sector_size(
     disk: *const DistinstDisk
 ) -> libc::uint64_t {
-    disk_action(disk, |disk| disk.get_sector_size())
+    let disk = &*(disk as *const Disk);
+    disk.get_sector_size()
 }
 
 #[no_mangle]
@@ -128,7 +110,8 @@ pub unsafe extern "C" fn distinst_disk_get_sector(
     disk: *const DistinstDisk,
     sector: *const DistinstSector,
 ) -> libc::uint64_t {
-    disk_action(disk, |disk| disk.get_sector(Sector::from(*sector)))
+    let disk = &*(disk as *const Disk);
+    disk.get_sector(Sector::from(*sector))
 }
 
 #[no_mangle]
@@ -136,24 +119,24 @@ pub unsafe extern "C" fn distinst_disk_mklabel(
     disk: *mut DistinstDisk,
     table: DISTINST_PARTITION_TABLE,
 ) -> libc::c_int {
+    let disk = &mut *(disk as *mut Disk);
+
     let table = match table {
         DISTINST_PARTITION_TABLE::GPT => PartitionTable::Gpt,
         DISTINST_PARTITION_TABLE::MSDOS => PartitionTable::Msdos,
         _ => return -1,
     };
 
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.mklabel(table) {
-            info!(
-                "unable to write partition table on {}: {}",
-                disk.path().display(),
-                why
-            );
-            -1
-        } else {
-            0
-        }
-    })
+    if let Err(why) = disk.mklabel(table) {
+        info!(
+            "unable to write partition table on {}: {}",
+            disk.path().display(),
+            why
+        );
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
@@ -161,14 +144,14 @@ pub unsafe extern "C" fn distinst_disk_add_partition(
     disk: *mut DistinstDisk,
     partition: *mut DistinstPartitionBuilder,
 ) -> libc::c_int {
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.add_partition(*Box::from_raw(partition as *mut PartitionBuilder)) {
-            info!("unable to add partition: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    let disk = &mut *(disk as *mut Disk);
+
+    if let Err(why) = disk.add_partition(*Box::from_raw(partition as *mut PartitionBuilder)) {
+        info!("unable to add partition: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
@@ -176,14 +159,14 @@ pub unsafe extern "C" fn distinst_disk_remove_partition(
     disk: *mut DistinstDisk,
     partition: libc::c_int,
 ) -> libc::c_int {
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.remove_partition(partition) {
-            info!("unable to remove partition: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    let disk = &mut *(disk as *mut Disk);
+
+    if let Err(why) = disk.remove_partition(partition) {
+        info!("unable to remove partition: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
@@ -192,14 +175,14 @@ pub unsafe extern "C" fn distinst_disk_resize_partition(
     partition: libc::c_int,
     end: libc::uint64_t,
 ) -> libc::c_int {
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.resize_partition(partition, end) {
-            info!("libdistinst: unable to resize partition: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    let disk = &mut *(disk as *mut Disk);
+
+    if let Err(why) = disk.resize_partition(partition, end) {
+        info!("libdistinst: unable to resize partition: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
@@ -208,14 +191,14 @@ pub unsafe extern "C" fn distinst_disk_move_partition(
     partition: libc::c_int,
     start: libc::uint64_t,
 ) -> libc::c_int {
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.move_partition(partition, start) {
-            info!("unable to remove partition: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    let disk = &mut *(disk as *mut Disk);
+
+    if let Err(why) = disk.move_partition(partition, start) {
+        info!("unable to remove partition: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
@@ -224,6 +207,8 @@ pub unsafe extern "C" fn distinst_disk_format_partition(
     partition: libc::c_int,
     fs: DISTINST_FILE_SYSTEM_TYPE,
 ) -> libc::c_int {
+    let disk = &mut *(disk as *mut Disk);
+
     let fs = match Option::<FileSystemType>::from(fs) {
         Some(fs) => fs,
         None => {
@@ -232,26 +217,24 @@ pub unsafe extern "C" fn distinst_disk_format_partition(
         }
     };
 
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.format_partition(partition, fs.clone()) {
-            info!("unable to remove partition: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    if let Err(why) = disk.format_partition(partition, fs.clone()) {
+        info!("unable to remove partition: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn distinst_disk_commit(disk: *mut DistinstDisk) -> libc::c_int {
-    disk_action_mut(disk, |disk| {
-        if let Err(why) = disk.commit() {
-            info!("unable to commit changes to disk: {}", why);
-            -1
-        } else {
-            0
-        }
-    })
+    let disk = &mut *(disk as *mut Disk);
+
+    if let Err(why) = disk.commit() {
+        info!("unable to commit changes to disk: {}", why);
+        -1
+    } else {
+        0
+    }
 }
 
 #[repr(C)]
