cff0f4c34b797310d2a5a648bfaf428b8d226b9a
Michael Aaron Murphy <mmstickman@gmail.com>
2018-03-08 13:12:09 -0500

    Theoretically support LVM device reuse Will need to implement it in the CLI to test whether it works or not

---
+++ tests/partition-lvm-reuse.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+FS="tests/filesystem.squashfs"
+REMOVE="tests/filesystem.manifest-remove"
+RUNS=3
+
+if ! test -e "target/debug/distinst"; then
+    cargo build --manifest-path cli/Cargo.toml
+fi
+
+if ! test "${1}"; then
+    echo "must provide a block device as an argument"
+    exit 1
+fi
+
+if ! test -b "${1}"; then
+    echo "'${1}' is not a block device"
+    exit 1
+fi
+
+for file in "$FS" "$REMOVE"; do
+    if ! test -e "${file}"; then
+        echo "failed to find ${file}"
+        exit 1
+    fi
+done
+
+set -x
+
+echo 'Running LVM on LUKS test'
+index=0; while test ${index} -ne ${RUNS}; do
+    sudo target/debug/distinst --test \
+        -s "${FS}" \
+        -r "${REMOVE}" \
+        -h "pop-testing" \
+        -b "$1" \
+        -t "$1:gpt" \
+        -n "$1:primary:start:512M:fat32:mount=/boot/efi:flags=esp" \
+        -n "$1:primary:512M:2048M:lvm=data" \
+        -n "$1:primary:2048M:end:lvm=data" \
+        --logical "data:root:-4096M:ext4:mount=/" \
+        --logical "data:swap:4096M:swap"
+
+    sudo target/debug/distinst --test \
+        -s "${FS}" \
+        -r "${REMOVE}" \
+        -h "pop-testing" \
+        -b "$1" \
+        -t "$1:gpt" \
+        -n "$1:primary:start:512M:fat32:mount=/boot/efi:flags=esp" \
+        -n "$1:primary:512M:2048M:lvm=data" \
+        -n "$1:primary:2048M:end:lvm=data" \
+        --logical-modify "data:root:fs=xfs:mount=/" \
+        --logical-remove "data:swap" \
+        --logical "data:other:4096M:ext4"
+
+    index=$((index + 1))
+done
