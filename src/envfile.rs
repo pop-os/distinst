154267d4b5a1d2ef994d0c2cee200d8bdf5d92da
Michael Murphy <mmstickman@gmail.com>
2018-05-18 10:53:16 -0600

    Add env_file write test

--- src/envfile.rs
+++ src/envfile.rs
@@ -62,23 +62,25 @@ impl<'a> EnvFile<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use misc;
     use tempdir::TempDir;
     use std::collections::BTreeMap;
     use std::fs::File;
     use std::io::Write;
 
-    const SAMPLE: &str = r#"HOSTNAME=pop-testing
-LANG=en_US.UTF-8
+    const SAMPLE: &str = r#"EFI_UUID=DFFD-D047
+HOSTNAME=pop-testing
 KBD_LAYOUT=us
 KBD_MODEL=
 KBD_VARIANT=
-EFI_UUID=DFFD-D047
+LANG=en_US.UTF-8
+OEM_MODE=0
 RECOVERY_UUID=8DB5-AFF3
 ROOT_UUID=2ef950c2-5ce6-4ae0-9fb9-a8c7468fa82c
-OEM_MODE=0"#;
+"#;
 
     #[test]
-    fn env_file() {
+    fn env_file_read() {
         let tempdir = TempDir::new("distinst_test").unwrap();
         let path = &tempdir.path().join("recovery.conf");
 
@@ -101,5 +103,22 @@ OEM_MODE=0"#;
             map.insert("OEM_MODE".into(), "0".into());
             map
         });
+    }
+
+    #[test]
+    fn env_file_write() {
+        let tempdir = TempDir::new("distinst_test").unwrap();
+        let path = &tempdir.path().join("recovery.conf");
+
+        {
+            let mut file = File::create(path).unwrap();
+            file.write_all(SAMPLE.as_bytes()).unwrap();
+        }
+
+        let mut env = EnvFile::new(path).unwrap();
+        env.write().unwrap();
+        let copy: &[u8] = &misc::read(path).unwrap();
+
+        assert_eq!(copy, SAMPLE.as_bytes());
     }
 }
