f846e128645b85a63aa19b55b673574b9edb812e
Michael Murphy <mmstickman@gmail.com>
2018-06-20 10:46:09 -0600

    Add unit test for hardware_support::modules

--- src/hardware_support/modules.rs
+++ src/hardware_support/modules.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Read};
 use std::fs::File;
 
+#[derive(Debug, PartialEq)]
 pub struct Module {
     pub name: String,
 }
@@ -19,17 +20,47 @@ impl Module {
         })
     }
 
+    pub fn parse_from<'a, I: Iterator<Item = &'a str>>(lines: I) -> io::Result<Vec<Module>> {
+        lines.map(Self::parse).collect()
+    }
+
     pub fn all() -> io::Result<Vec<Module>> {
-        let mut modules = Vec::new();
+        let file = File::open("/proc/modules")
+            .and_then(|mut file| {
+                let length = file.metadata().ok().map_or(0, |x| x.len() as usize);
+                let mut string = String::with_capacity(length);
+                file.read_to_string(&mut string).map(|_| string)
+            })?;
 
-        let mut data = String::new();
-        File::open("/proc/modules").and_then(|mut file| file.read_to_string(&mut data))?;
+        Self::parse_from(file.lines())
+    }
+}
 
-        for line in data.lines() {
-            let module = Module::parse(line)?;
-            modules.push(module);
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        Ok(modules)
+    const SAMPLE: &'static str = r#"snd_hda_intel 40960 9 - Live 0x0000000000000000
+snd_hda_codec 126976 4 snd_hda_codec_hdmi,snd_hda_codec_realtek,snd_hda_codec_generic,snd_hda_intel, Live 0x0000000000000000
+snd_hda_core 81920 5 snd_hda_codec_hdmi,snd_hda_codec_realtek,snd_hda_codec_generic,snd_hda_intel,snd_hda_codec, Live 0x0000000000000000
+nvidia_drm 40960 11 - Live 0x0000000000000000 (POE)
+nvidia_modeset 1085440 20 nvidia_drm, Live 0x0000000000000000 (POE)
+nvidia 14012416 938 nvidia_uvm,nvidia_modeset, Live 0x0000000000000000 (POE)
+video 40960 0 - Live 0x0000000000000000 (E)"#;
+
+    #[test]
+    fn modules() {
+        assert_eq!(
+            Module::parse_from(SAMPLE.lines()).unwrap(),
+            vec![
+                Module { name: "snd_hda_intel".into() },
+                Module { name: "snd_hda_codec".into() },
+                Module { name: "snd_hda_core".into() },
+                Module { name: "nvidia_drm".into() },
+                Module { name: "nvidia_modeset".into() },
+                Module { name: "nvidia".into() },
+                Module { name: "video".into() },
+            ]
+        )
     }
 }
