303740c925b83e689a6cb080bbd5ce6208642df8
Michael Aaron Murphy <mmstickman@gmail.com>
2018-09-11 12:19:42 -0600

    Support interaction with the dbus systemd login1 manager
    UIs will no longer need to write their own code to inhibit suspension on systemd systems.

---
+++ ffi/src/dbus.rs
@@ -0,0 +1,21 @@
+use distinst::dbus_interfaces::LoginManager;
+use libc;
+
+#[no_mangle]
+pub extern "C" fn distinst_session_inhibit_suspend() -> libc::c_int {
+    let manager = match LoginManager::new() {
+        Ok(manager) => manager,
+        Err(why) => {
+            error!("failed to get logind manager: {}", why);
+            return -1;
+        }
+    };
+
+    match manager.connect().inhibit_suspend() {
+        Ok(pipe_fd) => pipe_fd.into_fd(),
+        Err(why) => {
+            error!("failed to suspend: {}", why);
+            return -1;
+        }
+    }
+}
