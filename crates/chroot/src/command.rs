3fa339c9c8b1c8f0616d21c48f5bf9d89cd71d67
Michael Aaron Murphy <mmstick@pm.me>
2019-04-18 15:06:49 -0600

    :ambulance: Fix high CPU usage during install

--- crates/chroot/src/command.rs
+++ crates/chroot/src/command.rs
@@ -1,9 +1,11 @@
 use libc;
-use std::process::{self, Child, ExitStatus, Stdio};
-use std::io::{self, BufRead, BufReader, Error, ErrorKind, Write};
 use std::ffi::OsStr;
-use std::os::unix::io::{FromRawFd, IntoRawFd};
 use std::fs::File;
+use std::io::{self, BufRead, BufReader, Error, ErrorKind, Write};
+use std::os::unix::io::{FromRawFd, IntoRawFd};
+use std::process::{self, Child, ExitStatus, Stdio};
+use std::thread;
+use std::time::Duration;
 
 /// Convenient wrapper around `process::Command` to make it easier to work with.
 pub struct Command<'a> {
@@ -111,6 +113,7 @@ impl<'a> Command<'a> {
         let mut stderr = child.stderr.take().map(non_blocking).map(BufReader::new);
 
         loop {
+            thread::sleep(Duration::from_millis(16));
             match child.try_wait()? {
                 Some(status) => return status_as_result(status, &cmd),
                 None => {
