0c446143521ce286588b9464faf66b6ce06969d8
Cappy Ishihara <cappy@cappuchino.xyz>
2024-04-15 23:53:17 +0700

    refactor mount ops in os-detect

--- crates/os-detect/src/lib.rs
+++ crates/os-detect/src/lib.rs
@@ -54,7 +54,10 @@ pub fn detect_os_from_device<'a, F: Into<FilesystemType<'a>>>(device: &Path, fs:
     TempDir::new("distinst").ok().and_then(|tempdir| {
         // Mount the FS to the temporary directory
         let base = tempdir.path();
-        Mount::new(device, base, fs, MountFlags::empty(), None)
+        Mount::builder()
+            .flags(MountFlags::empty())
+            .fstype(fs)
+            .mount(device, base)
             .map(|m| m.into_unmount_drop(UnmountFlags::DETACH))
             .ok()
             .and_then(|_mount| detect_os_from_path(base))
