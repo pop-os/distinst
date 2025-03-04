82480b911e1e6f712cc2efb6c7476e8f48b851ef
Michael Aaron Murphy <michael@mmurphy.dev>
2025-03-04 19:04:42 +0100

    fix(hardware): default to hybrid graphics on switchable systems

--- crates/hardware/src/switchable_graphics.rs
+++ crates/hardware/src/switchable_graphics.rs
@@ -1,10 +1,5 @@
 use dbus::blocking::Connection;
-use std::{
-    fs,
-    io,
-    path::Path,
-    time::Duration,
-};
+use std::{fs, io, path::Path, time::Duration};
 
 const POWER: &str = "etc/modprobe.d/system76-power.conf";
 const PRIME_DISCRETE: &str = "etc/prime-discrete";
@@ -36,43 +31,24 @@ pub fn configure_graphics(mount_dir: &Path) -> io::Result<bool> {
 
     let _ = fs::create_dir_all(mount_dir.join("etc/modprobe.d/"));
 
-    match default_graphics().as_str() {
-        "integrated" => {
-            info!("disabling external NVIDIA graphics by default");
-            fs::write(mount_dir.join(POWER), MODPROBE_INTEGRATED)?;
+    info!("settings module options for hybrid graphics mode");
+    fs::write(mount_dir.join(POWER), MODPROBE_HYBRID)?;
 
-            info!("configuring gpu-manager for integrated graphics mode");
-            fs::write(mount_dir.join(PRIME_DISCRETE), "off")?;
-        },
-        "hybrid" => {
-            info!("settings module options for hybrid graphics mode");
-            fs::write(mount_dir.join(POWER), MODPROBE_HYBRID)?;
-
-            info!("configuring gpu-manager for hybrid graphics mode");
-            fs::write(mount_dir.join(PRIME_DISCRETE), "on-demand")?;
-        },
-        _ => (),
-    }
+    info!("configuring gpu-manager for hybrid graphics mode");
+    fs::write(mount_dir.join(PRIME_DISCRETE), "on-demand")?;
 
     Ok(true)
 }
 
-fn default_graphics() -> String {
-    if let Ok(conn) = Connection::new_system() {
-        let proxy = conn.with_proxy("com.system76.PowerDaemon", "/com/system76/PowerDaemon", Duration::from_millis(1000));
-        let (gfx,): (String,) = proxy.method_call("com.system76.PowerDaemon", "GetDefaultGraphics", ())
-            .unwrap_or_else(|_| ("nvidia".to_string(), ));
-
-        gfx
-    } else {
-        "nvidia".to_string()
-    }
-}
-
 fn has_switchable_graphics() -> bool {
     if let Ok(conn) = Connection::new_system() {
-        let proxy = conn.with_proxy("com.system76.PowerDaemon", "/com/system76/PowerDaemon", Duration::from_millis(1000));
-        let (switchable,): (bool,) = proxy.method_call("com.system76.PowerDaemon", "GetSwitchable", ()).unwrap_or_default();
+        let proxy = conn.with_proxy(
+            "com.system76.PowerDaemon",
+            "/com/system76/PowerDaemon",
+            Duration::from_millis(1000),
+        );
+        let (switchable,): (bool,) =
+            proxy.method_call("com.system76.PowerDaemon", "GetSwitchable", ()).unwrap_or_default();
 
         // Limit configuring graphics to System76 models
         switchable && (system_vendor() == "System76")
@@ -82,8 +58,5 @@ fn has_switchable_graphics() -> bool {
 }
 
 fn system_vendor() -> String {
-    fs::read_to_string("/sys/class/dmi/id/sys_vendor")
-        .unwrap_or_default()
-        .trim()
-        .into()
+    fs::read_to_string("/sys/class/dmi/id/sys_vendor").unwrap_or_default().trim().into()
 }
